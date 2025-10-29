3d589f7b7a03b014154042c7cb9967a7730e9ddc
Joshua <joshuadejesus12345@gmail.com>
2025-10-30 07:03:25 +0800

    typo that breaks login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -11,7 +11,7 @@ export default function Login (){
     const handleSubmit = async(e) => {
         e.preventDefault();
         try {
-            const res = await fetch(`${API_BASE}/api/login"`, {
+            const res = await fetch(`${API_BASE}/api/login`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             credentials: "include",
